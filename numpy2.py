ea40a27968760af963f931fcb4cafe3d1332cb81
Abinaya00007 <acharyaabinaya12@gmail.com>
2024-09-28 13:34:22 +0545

    Create numpy2.py

---
+++ numpy2.py
@@ -0,0 +1,8 @@
+import numpy as np
+# Create an array of zeros
+zeros_array = np.zeros((2, 3))  # Shape (rows, columns)
+print(zeros_array)
+
+# Create an array of ones
+ones_array = np.ones((3, 2))
+print(ones_array)
