c4df03a88055c92d54be8f4b65e92eb366f9b12e
Abinaya00007 <acharyaabinaya12@gmail.com>
2024-10-01 09:57:41 +0545

    Update getsetdelete.py

--- getsetdelete.py
+++ getsetdelete.py
@@ -1,1 +1,26 @@
 
+class Car:
+    def __init__(self,name,color):
+        self.name=name
+        self.color=color
+    
+    def set(self):
+        name=input("enter the name")
+        color=input("enter the color")
+        self.name=name
+        self.color=color 
+        
+    def get(self):
+        print(self.name,self.color) 
+        
+    def delete(self):
+       del self.name
+        
+car=Car("abinaya","red")
+car.get()
+
+car.set()
+car.get()
+
+car.delete()
+print(car.__dict__)
